1f58053e46110db8bc16fcb09fda6eb0115cd8db
Yuliya Bryukhovskikh <makutu@ereb.ru>
2023-04-17 14:52:49 +0200

    установила и настроила Flask-SQLAlchemy

--- blog/app.py
+++ blog/app.py
@@ -3,6 +3,7 @@ from werkzeug.exceptions import BadRequest
 from flask import Flask, request, g, render_template
 from blog.views.users import users_app
 from blog.views.articles import articles_app
+from blog.models.database import db
 
 
 app = Flask(__name__)
@@ -94,5 +95,8 @@ def handle_zero_division_error(error):
 
 app.register_blueprint(users_app, url_prefix="/users")
 app.register_blueprint(articles_app, url_prefix="/articles")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/blog.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
 
 
