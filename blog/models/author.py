0abae6de703c983621e0b2c119cbec6ac42ab8a3
Yuliya Bryukhovskikh <makutu@ereb.ru>
2023-04-25 22:54:46 +0200

    создала модель автора и статей, их связи, вывела список статей на страницу и добавила переход к просмотру полной статьи

---
+++ blog/models/author.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from blog.models.database import db
+
+
+class Author(db.Model):
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+    user = relationship("User", back_populates="author")
+    articles = relationship("Article", back_populates="author")
---
+++ blog/views/authors.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template
+from blog.models import Author
+
+
+authors_app = Blueprint("authors_app", __name__)
+
+
+@authors_app.route("/", endpoint="list")
+def authors_list():
+    authors = Author.query.all()
+    return render_template("authors/list.html", authors=authors)
