dc30cb8675e79778b6cb6beb29e5e4a75bbdb22e
Yuliya Bryukhovskikh <makutu@ereb.ru>
2023-04-28 16:07:30 +0200

    создала таблицу

--- blog/models/tag.py
+++ blog/models/tag.py
@@ -1,8 +1,15 @@
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from blog.models.article_tag import article_tag_association_table
 from blog.models.database import db
 
 
 class Tag(db.Model):
     id = Column(Integer, primary_key=True)
     name = Column(String(32), nullable=False, default="", server_default="")
+    articles = relationship(
+        "Article",
+        secondary=article_tag_association_table,
+        back_populates="tags",
+    )
 
