75353ce53108b212735c4b224b0e4f686466300c
Yuliya Bryukhovskikh <makutu@ereb.ru>
2023-04-28 16:34:35 +0200

    создала и добавила теги

--- blog/forms/article.py
+++ blog/forms/article.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField, validators
+from wtforms import StringField, TextAreaField, SubmitField, SelectMultipleField,  validators
 
 
 class CreateArticleForm(FlaskForm):
@@ -12,3 +12,5 @@ class CreateArticleForm(FlaskForm):
         [validators.DataRequired()],
     )
     submit = SubmitField("Publish")
+    tags = SelectMultipleField("Tags", coerce=int)
+
--- blog/views/articles.py
+++ blog/views/articles.py
@@ -4,7 +4,7 @@ from sqlalchemy.exc import IntegrityError
 from werkzeug.exceptions import NotFound
 
 from blog.models.database import db
-from blog.models import Author, Article
+from blog.models import Author, Article, Tag
 from blog.forms.article import CreateArticleForm
 
 
@@ -20,9 +20,15 @@ def articles_list():
     return render_template("articles/list.html", articles=articles)
 
 
+def joinedload(tags):
+    pass
+
+
 @articles_app.route("/<int:article_id>/", endpoint="details")
 def article_details(article_id):
-    article = Article.query.filter_by(id=article_id).one_or_none()
+    article = Article.query.filter_by(id=article_id).options(
+        joinedload(Article.tags)  # подгружаем связанные теги!
+    ).one_or_none()
     if article is None:
         raise NotFound
     return render_template("articles/details.html", article=article)
@@ -33,9 +39,15 @@ def article_details(article_id):
 def create_article():
     error = None
     form = CreateArticleForm(request.form)
-    if request.method == "POST" and form.validate_on_submit():
+    # добавляем доступные теги в форму
+    form.tags.choices = [(tag.id, tag.name) for tag in Tag.query.order_by("name")]
+    if request.method == "POST" and form.validate_on_submit():  # при создании статьи
         article = Article(title=form.title.data.strip(), body=form.body.data)
         db.session.add(article)
+        if form.tags.data:  # если в форму были переданы теги (были выбраны)
+            selected_tags = Tag.query.filter(Tag.id.in_(form.tags.data))
+        for tag in selected_tags:
+            article.tags.append(tag)  # добавляем выбранные теги к статье
         if current_user.author:
             # use existing author if present
             article.author = current_user.author
